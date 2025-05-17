3921edac9ae3e39b5ac5a53ccffbe1e3d6706f47
Pablo Zuval <pablo.zuniga.vale@gmail.com>
2025-05-16 20:02:44 -0400

    📦 Setup: Project files and dependencies

---
+++ diccionario_emociones.py
@@ -0,0 +1,156 @@
+diccionario_emociones = {
+  "feliz": {
+    "alegría": 2,
+
+  },
+  "contento": {
+    "alegría": 2
+  },
+  "alegre": {
+    "alegría": 2
+  },
+  "satisfecho": {
+    "alegría": 2
+  },
+  "encantado": {
+    "alegría": 2
+  },
+  "maravilloso": {
+    "alegría": 2
+  },
+  "agradecido": {
+    "alegría": 1
+  },
+  "divertido": {
+    "alegría": 1
+  },
+  "optimista": {
+    "alegría": 1
+  },
+  "pleno": {
+    "alegría": 1
+  },
+  "emocionado": {
+    "alegría": 1
+  },
+  "entusiasmado": {
+    "tristeza": -1
+  },
+  "disfruté": {
+    "alegría": 2
+  },
+  "disfrutar": {
+    "alegría": 1
+  },
+  "disfruto": {
+    "alegría": 2
+  },
+  "triste": {
+    "tristeza": 2
+  },
+  "deprimido": {
+    "tristeza": 2
+  },
+  "melancólico": {
+    "tristeza": 2
+  },
+  "llorando": {
+    "tristeza": 2
+  },
+  "sonreir":{
+      "alegría": 2
+  },
+  "abatido": {
+    "tristeza": 1
+  },
+  "desmotivado": {
+    "tristeza": 1
+  },
+  "apagado": {
+    "tristeza": 1
+  },
+  "enojado": {
+    "enojo": 2
+  },
+  "molesto": {
+    "enojo": 2
+  },
+  "furioso": {
+    "enojo": 2
+  },
+  "rabia": {
+    "enojo": 2
+  },
+  "irritado": {
+    "enojo": 2
+  },
+  "harto": {
+    "enojo": 1
+  },
+  "exasperado": {
+    "enojo": 1
+  },
+  "miedo": {
+    "miedo": 2
+  },
+  "temor": {
+    "miedo": 2
+  },
+  "asustado": {
+    "miedo": 2
+  },
+  "ansioso": {
+    "miedo": 1
+  },
+  "nervioso": {
+    "miedo": 1
+  },
+  "paranoico": {
+    "miedo": 1
+  },
+  "asco": {
+    "asco": 2
+  },
+  "repulsión": {
+    "asco": 2
+  },
+  "repugnante": {
+    "asco": 2
+  },
+  "nauseabundo": {
+    "asco": 2
+  },
+  "asqueroso": {
+    "asco": 2
+  },
+  "náuseas": {
+    "asco": 2
+  },
+  "rechazo": {
+    "asco": 1
+  },
+  "sorpresa": {
+    "sorpresa": 2
+  },
+  "sorprendente": {
+    "sorpresa": 2
+  },
+  "impresionado": {
+    "sorpresa": 2
+  },
+  "inesperado": {
+    "sorpresa": 2
+  },
+  "desprevenido": {
+    "sorpresa": 1
+  },
+  "impactado": {
+    "sorpresa": 1
+  },
+  "asombrado": {
+    "sorpresa": 2
+  },
+  "repugnado": {
+      "asco": 2
+      },
+}
